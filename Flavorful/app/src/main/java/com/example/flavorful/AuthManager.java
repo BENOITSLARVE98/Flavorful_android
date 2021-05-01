4439ef598d66bb44fc81b97d34446ca779f89408
BenoitSlarve98 <benoitslarve98@gmail.com>
2021-05-01 14:35:30 -0400

    Save Profile Images to storage and firestore
    Save Profile Images to storage and firestore

--- Flavorful/app/src/main/java/com/example/flavorful/AuthManager.java
+++ Flavorful/app/src/main/java/com/example/flavorful/AuthManager.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -15,6 +16,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -26,14 +30,18 @@ public class AuthManager {
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseUser currentUser = mAuth.getCurrentUser();
+    Map<String, Object> users = new HashMap<>();
+
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    StorageReference storageRef = storage.getReference();
 
     public void saveUser(Uri image, String name, String email) {
 
         if(currentUser != null){
-            Map<String, Object> users = new HashMap<>();
             users.put("uid", currentUser.getUid());
             users.put("name", name);
             users.put("email", email);
+            users.put("profileImageUrl", "");
 
             db.collection("users").document(currentUser.getUid())
                     .set(users)
@@ -41,6 +49,7 @@ public class AuthManager {
                         @Override
                         public void onSuccess(Void aVoid) {
                             Log.d("TAG", "DocumentSnapshot successfully written!");
+                            saveImage(image);
                         }
                     })
                     .addOnFailureListener(new OnFailureListener() {
@@ -50,6 +59,40 @@ public class AuthManager {
                         }
                     });
         }
+
+    }
+
+    public void saveImage(Uri image) {
+        StorageReference storageProfileRef = storageRef.child("profileImages").child(currentUser.getUid());
+        UploadTask uploadTask = storageProfileRef.putFile(image);
+
+        //Save image to firebase Storage
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+
+                // Continue with the task to get the download URL
+                return storageProfileRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+                    users.put("profileImageUrl", downloadUri.toString());
+
+                    //Save user to FireStore user collection
+                    db.collection("users").document(currentUser.getUid()).update(users);
+
+                } else {
+                    // Handle failures
+                    // ...
+                }
+            }
+        });
 
     }
 }
